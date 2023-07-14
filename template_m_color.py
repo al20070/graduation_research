ab19430b8207407662d9669353929c555583e41e
al20070 <106135839+al20070@users.noreply.github.com>
2023-07-14 13:10:04 +0900

    add file

--- template_m_color.py
+++ template_m_color.py
@@ -1,5 +1,5 @@
-#動画内のバブルの位置をテンプレートマッチングで検出
-#バブルを四角で囲い上に座標表示
+#動画内のバブル(視聴者の視線が集中する場所)を求める
+#
 #動画のサイズはresizeで1920×1080にする
 #結果がウィンドウに収まらない、処理が遅い場合はテンプレート画像と動画サイズを小さくする
 
@@ -9,14 +9,6 @@ import numpy as np
 cap = cv2.VideoCapture('./video.mkv')
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-template = cv2.imread("template.png")
-#テンプレート画像のサイズ縮小(結果がウィンドウに収まらない、処理が遅い場合)
-temp_w, temp_h, _ = template.shape
-temp_w = int(temp_w/2)
-temp_h = int(temp_h/2)
-template = cv2.resize(template, (temp_w, temp_h))
-#ここまで
 
 while(1):
     # 一フレーム取り出す
@@ -44,18 +36,11 @@ while(1):
     erode = cv2.erode(bit_mask, kernel)
     dilate = cv2.dilate(erode, kernel)
 
-    #バブルの座標表示
-    match = cv2.matchTemplate(dilate, template, cv2.TM_CCORR_NORMED)
-    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match)
-    left = max_loc
-    right = (left[0] + temp_w, left[1] + temp_h)
-    cv2.rectangle(frame, left, right, (255, 0, 0), 2)
-    text = str(left[0] + int(temp_w/2)) + "," + str(left[1] + int(temp_h/2))
-    org = (max_loc[0], max_loc[1])
-    cv2.putText(frame, text, org, fontFace = cv2.FONT_HERSHEY_PLAIN, fontScale = 1.5, color = (255, 0, 0))
+    blur = cv2.GaussianBlur(mask,(25,25),0)
 
     #結果動画表示
-    cv2.imshow('frame',frame)               #元動画
+    #cv2.imshow('frame',frame)               #元動画
+    cv2.imshow('frame',blur)               #元動画
     #cv2.imshow('mask',mask)                #マスクに対応する部分表示(2値) 
     #cv2.imshow('result',bit_mask)          #マスクをかけた動画
     #cv2.imshow('result -noizu',dilate)     #ノイズ除去後の動画    
